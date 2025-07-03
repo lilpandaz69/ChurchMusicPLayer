b1cccdebeb163985abfb5da2bca65b99a79a5c57
FIshz7 <bavlymina125@gmail.com>
2025-07-03 18:14:07 +0300

    database

--- src/sql/music_db.sql
+++ src/sql/music_db.sql
@@ -20,7 +20,8 @@ CREATE TABLE IF NOT EXISTS users (
 
 
 INSERT INTO songs (title, artist, file_path , file_Pathp) VALUES
-('kalmeny belel', 'marwan mousa', 'Marwaa.mp3','download(2).jpg'),
-('el wa2t el daye3', 'marwan mousa', 'Lege-Cy - Elwa2t Eldaye3  ليجي سي - الوقت الضايع (Official Audio).mp3', 'download(3).jpg'),
-('kalmeny belel', 'marwan mousa', 'Lella Fadda x @Abyusif - ATTA3 للا فضة و ابيوسف - قطع.mp3', 'download(4).jpg'),
-('Graceful Light', 'Choir B', 'Lege-Cy - El Neyya  ليجي-سي - النيه (Official Audio).mp3', 'download(5).jpg');
+('احفظ حياتي', 'منال متري', 'احفظ حياتي .mp3','download (7).jpg'),
+('احلي حبيب', 'ابونا يوسف اسعد', 'احلى حبيب.MP3', 'download.jpg'),
+('ارض افرحي', 'فريق المس ايدينا', 'ارض افرحي.mp3', 'download(1).jpg'),
+('اسألوني عن يسوع', 'ابونا يوسف اسعد', 'اسآلونى عن يسوع.MP3', 'download(2).jpg');
+('استيقظي', 'تاسوني / ماري عزيز', 'استيقظي.mp3', 'download(3).jpg');
